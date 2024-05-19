2340a13d9f6fe9474af31d8986760ad4680d71a4
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-05-19 15:54:45 +0600

    vehicle and membership table added

---
+++ database/migrations/2024_05_16_150913_create_memberships_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 191);
+            $table->string('contact_number', 191);
+            $table->string('type', 191);
+            $table->integer('points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
