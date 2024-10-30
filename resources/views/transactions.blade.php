dd6d16923451a10df370c5b0e78725ecd81f74ee
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-10-30 18:15:09 +0600

    size updated

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -17,47 +17,51 @@
     }
 </style>
 <body style="font-family:'Noto Sans Bengali', sans-serif; margin: 20px;">
-<h1 style="text-align: center;">Date-wise Report</h1>
+<h1 style="text-align: center;">Transaction Report</h1>
 
 <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
     <thead>
     <tr>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">SL No.</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Vehicle</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Date</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Payable</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Paid</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Discount</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Due</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Type</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Status</th>
-        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; background-color: #f2f2f2;">Method</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">SL No.</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Vehicle</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Date</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Payable</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Paid</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Discount</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Due</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Type</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Status</th>
+        <th style="border: 1px solid #ddd; font-size:12px; text-align: center; background-color: #f2f2f2;">Method</th>
     </tr>
     </thead>
     <tbody>
         @foreach ($transactions as $item)
             <tr>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">
                     {{$loop->index + 1}}
                 </td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{$item->vehicle?->number}}</td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{$item->transaction_date}}</td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{'৳ '.$item->total_payable}}</td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{'৳ '.$item->total_paid}}</td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{$item->vehicle?->number}}</td>
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{$item->transaction_date}}</td>
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{'৳ '.$item->total_payable}}</td>
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{'৳ '.$item->total_paid}}</td>
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">
                     <div style="display: flex; flex-direction: column; align-items: end">
                         @if($item->membership_discount)
-                        <div>{{'৳ '.$item->membership_discount}}</div>
+                        <div>{{'Membership: ৳ '.$item->membership_discount}}</div>
                         @endif
                         @if($item->discount_amount)
-                        <div>{{'৳ '.$item->discount_amount}}</div>
+                        <div>{{'Other ৳ '.$item->discount_amount}}</div>
                         @endif
                     </div>
                 </td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{'৳ '.$item->total_due}}</td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{$item->payment_type}}</td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{$item->status}}</td>
-                <td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{{$item->method}}</td>
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{'৳ '.$item->total_due}}</td>
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{$item->payment_type}}</td>
+                @if($item->status != 'success')
+                    <td style="border: 1px solid #ddd; font-size:12px; text-align: center; background: orange; color: white;">{{$item->status}}</td>
+                @else
+                    <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{$item->status}}</td>
+                @endif
+                <td style="border: 1px solid #ddd; font-size:12px; text-align: center;">{{$item->method}}</td>
             </tr>
         @endforeach
     </tbody>
